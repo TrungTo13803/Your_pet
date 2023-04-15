c95bbe74548482ac4a2772cf5aa0c752e59367a1
its-hmtu <tu.hoangminh15@gmail.com>
2023-04-15 10:58:11 +0700

    small changed - Error btn color - add auth-exceptions

--- lib/auth/auth_exceptions.dart
+++ lib/auth/auth_exceptions.dart
@@ -1,1 +1,16 @@
-import 'package:flutter/material.dart';
+// login exceptions
+class UserNotFoundAuthExceptions implements Exception {}
+
+class WrongPasswordAuthExceptions implements Exception {}
+
+// register exceptions
+class WeakPasswordAuthExceptions implements Exception {}
+
+class EmailAlreadyInUseAuthExceptions implements Exception {}
+
+class InvalidEmailAuthExceptions implements Exception {}
+
+// general exceptions
+class GeneralAuthExceptions implements Exception {}
+
+class UserNotLoggedInAuthExceptions implements Exception {}
